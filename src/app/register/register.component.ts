9ca3df808f03a1d728ee3197e6b3b1567d45b0a5
febrisyafriwan <febrisyafriwan@gmail.com>
2020-06-20 01:48:24 +0700

    4 files modified, 1 added, 1 deleted

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { first } from 'rxjs/operators';
-
+import { Component } from "@angular/core";
+import { first } from "rxjs/operators";
+import { FormControl, Validators } from "@angular/forms";
 @Component({
   selector: "register",
   templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"]
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent {
-    constructor() {}
-
-    ngOnInit() {
-      
-    }
+  constructor() {}
+  emailFormControl = new FormControl("", [
+    Validators.required,
+    Validators.email
+  ]);
+  ngOnInit() {}
 }
