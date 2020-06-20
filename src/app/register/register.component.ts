7fb3e631f408b5c9392a2c35c673c2fd286d0dd4
febrisyafriwan <febrisyafriwan@gmail.com>
2020-06-20 15:34:05 +0700

    2 files modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,18 +1,29 @@
 import { Component } from "@angular/core";
 import { first } from "rxjs/operators";
 import { FormGroup , Validators , FormBuilder,AbstractControl} from '@angular/forms';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  AbstractControl,
+} from "@angular/forms";
 class Registration {
-  username = '';
-  password = ''
+  username = "";
+  password = "";
 }
 @Component({
   selector: "register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.css"]
 })
-
 export class RegisterComponent {
-  constructor() {}
+  loginform:FormControl
+  constructor(public fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginform = this.fb.group({
+      name: ["", [Validators.required, cvalidate]],
+      password: ["", [Validators.required, cvMinlength(5)]]
+    });
+  }
 }
